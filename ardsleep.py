0b2f3fd6305b96c77c31d57fe90820ab52c30fe4
san <san@sancomp>
2017-01-18 23:20:15 +0300

    sl p3

--- ardsleep.py
+++ ardsleep.py
@@ -4,6 +4,6 @@ bus = smbus.SMBus(1)
 try:
 	stime = int(sys.argv[1])
 	bus.write_i2c_block_data(0x05, 1, [stime >> 8, stime & 0xFF])
-	print json.dumps({'success': True})
+	print(json.dumps({'success': True}))
 except:
-	print json.dumps({'success': False})
+	print(json.dumps({'success': False}))
