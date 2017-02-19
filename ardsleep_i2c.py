affb6cb2b80179db77075df27a9a9eba07bd2aee
san <san@sansystem>
2017-02-19 16:03:24 +0300

    i2c fix

--- ardsleep_i2c.py
+++ ardsleep_i2c.py
@@ -7,7 +7,7 @@ try:
 	data = bus.read_i2c_block_data(0x05, 0, 1)
 	if data[0] == 1:
 		print(json.dumps({'success': True}))
-	else if data[0] == 2:
+	elif data[0] == 2:
 		print(json.dumps({'success': False}))
 		print('ok hand err')
 except:
