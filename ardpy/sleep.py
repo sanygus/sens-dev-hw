3201cb76a6d6ba0e060a1dbebe9af63d13a3c6d7
sanygus <sashagusarov@mail.ru>
2017-10-16 09:39:53 +0300

    syntax

--- ardpy/sleep.py
+++ ardpy/sleep.py
@@ -5,13 +5,13 @@ for i in range(5):
   res = {}
   try:
     bus = smbus.SMBus(1)
-  	bus.write_i2c_block_data(0x05, 2, [(stime >> 8) & 0xFF, stime & 0xFF])
-  	data = bus.read_i2c_block_data(0x05, 0, 1)
+    bus.write_i2c_block_data(0x05, 2, [(stime >> 8) & 0xFF, stime & 0xFF])
+    data = bus.read_i2c_block_data(0x05, 0, 1)
     bus.close()
-  	if data[0] == 1:
-  		res = {'success': True}
+    if data[0] == 1:
+      res = {'success': True}
       break
-  	elif data[0] == 2:
+    elif data[0] == 2:
       res = {'success': False}
   except:
     res = {'success': False}
