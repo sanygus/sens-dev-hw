e7cc6a4e9a59d382190fbbcd3f605fc453fad044
sanygus <sashagusarov@mail.ru>
2018-03-29 17:03:00 +0300

    comments

--- libraries/Volt/Volt.cpp
+++ libraries/Volt/Volt.cpp
@@ -5,17 +5,19 @@ Volt::Volt(int pin)
 {
   _pin = pin;
 }
-
+//обновляет текущее значений вольтажа, пересчитывает среднее значение
 void Volt::readVolt()
 {
   unsigned int sum = 0;
   int countNotNull = 0;
   for (int i = 1; i < countValues; i++) {
+    //сдвигаем текущие значения
     _values[i - 1] = _values[i];
     if (_values[i] > 0) {
       sum += _values[i];
       countNotNull++;
     }
+    //обновляем последний элемент текущим значением
     if (i == (countValues - 1)) {
       _values[i] = analogRead(_pin);
       if (_values[i] > 0) {
@@ -38,6 +40,7 @@ unsigned int Volt::getVolt()
 
 float Volt::getCharge(unsigned int min, unsigned int max)
 {
+  //перевод текущего значения АЦП в % заряда в диапазоне min max
   float charge = 0;
   if ((_value >= min) && (_value <= max)) {
     charge = ((float)(_value - min) / (float)(max - min));
